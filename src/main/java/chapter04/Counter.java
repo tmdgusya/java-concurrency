6689ccf5c254e45cfdd00aa0ff817e7b1d4177fb
정승현 <tmdgusya@naver.com>
2023-03-20 21:42:19 +0900

    implement Counter class

---
+++ src/main/java/chapter04/Counter.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+import annotation.ThreadSafe;
+
+@ThreadSafe
+public class Counter {
+    private long value = 0;
+
+    public synchronized long getValue() {
+        return value;
+    }
+
+    public synchronized long increment() {
+        if (value == Long.MAX_VALUE) {
+            throw new IllegalStateException("counter overflow");
+        }
+        return ++value;
+    }
+}
